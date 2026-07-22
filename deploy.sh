3e935425f6645808f216187a490a4733c92c1a94
damilaresanni <sannidamilare9@gmail.com>
2026-07-22 11:02:29 +0000

    fix: upate

--- deploy.sh
+++ deploy.sh
@@ -5,8 +5,6 @@ git reset --hard origin/main
 python3 -m venv server
 source server/bin/activate
 pip install -r requirements.txt
-which python
-pip list
 sudo systemctl restart house-pred-api.service
 sleep 2
 curl -f http://localhost:8000/health || (echo "Health check failed" && exit 1)
