7aadbc41a0a8466c6ade60208f9976fdca64cbd2
damilaresanni <sannidamilare9@gmail.com>
2026-07-22 08:19:57 +0000

    feat(ci-cd): add virtual env creation to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,11 @@ set -e
 cd /opt/house-pred-api/house-pred-mlops/
 git fetch origin main
 git reset --hard origin/main
-source .venv/bin/activate
+python3 -m venv deploy
+source deploy/bin/activate
 pip install -r requirements.txt
+which python
+pip list
 sudo systemctl restart house-pred-api.service
 sleep 2
 curl -f http://localhost:8000/health || (echo "Health check failed" && exit 1)
