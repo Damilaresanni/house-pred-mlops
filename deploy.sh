a17f1d67cf6d7fb38bb8afc985c357f3d07947bc
damilaresanni <sannidamilare9@gmail.com>
2026-07-23 13:11:00 +0000

    fix: remove uneccesary command

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,5 @@ python3 -m venv server
 source server/bin/activate
 pip install -r requirements.txt
 sudo systemctl restart house-pred-api
-sleep 2
-curl -f http://localhost:8000/health || (echo "Health check failed" && exit 1)
 echo "This deployment script run success"
 
